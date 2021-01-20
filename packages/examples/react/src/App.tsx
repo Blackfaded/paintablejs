2d7f78fbfa71c4a661151af22a1d717c784ef420
René Heinen <r.heinen@nanogiants.de>
2021-01-20 09:08:35 +0100

    refactor: add smoothness to react

--- packages/examples/react/src/App.tsx
+++ packages/examples/react/src/App.tsx
@@ -4,13 +4,15 @@ import styles from './App.module.css';
 
 function App() {
   const paintableRef = useRef<PaintableRef>(null);
-  const [color, setColor] = useState('blue');
+  const [color, setColor] = useState('#0000FF');
+  const [smooth, setSmooth] = useState(false);
   const [active, setActive] = useState(false);
   const [thickness, setThickness] = useState(5);
   const [useEraser, setUseEraser] = useState(false);
   return (
     <div>
       <div>
+        <button onClick={() => paintableRef.current?.clear()}>Clear</button>
         <button onClick={() => paintableRef.current?.undo()}>Undo</button>
         <button onClick={() => paintableRef.current?.redo()}>Redo</button>
         <button
@@ -24,6 +26,17 @@ function App() {
         <button onClick={() => setUseEraser(!useEraser)}>
           {useEraser ? 'use pencil' : 'use eraser'}
         </button>
+        <label>
+          Smooth:
+          <input
+            type="checkbox"
+            checked={smooth}
+            onChange={(e) => {
+              console.log(e.target.checked);
+              setSmooth(e.target.checked);
+            }}
+          />
+        </label>
         <input type="color" onChange={(e) => setColor(e.target.value)} />
         <input
           type="range"
@@ -44,6 +57,7 @@ function App() {
         useEraser={useEraser}
         ref={paintableRef}
         image={localStorage.getItem('/')}
+        smooth={smooth}
         onSave={(image: string) => localStorage.setItem('/', image)}
         onLongPress={() => console.log('long')}
       >
