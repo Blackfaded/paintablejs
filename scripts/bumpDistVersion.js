de2554aaa4f3690be828bd7184b6a692e4563c7c
René Heinen <r.heinen@nanogiants.de>
2021-01-22 19:52:46 +0100

    build: update all paintable dependencies on version bump

--- scripts/bumpDistVersion.js
+++ scripts/bumpDistVersion.js
@@ -1,9 +1,26 @@
 const fs = require('fs');
+const globby = require('globby');
 const mainPackageJson = require('../package.json');
 
 const distPackageJson = require('../packages/dist/package.json');
 
 distPackageJson.version = mainPackageJson.version;
+
+globby([
+  'packages/**/*/package.json',
+  '!**/*/node_modules/**/*/package.json',
+]).then((paths) => {
+  for (const path of paths) {
+    const packageJsonFile = JSON.parse(fs.readFileSync(path, 'utf8'));
+    if (
+      packageJsonFile.dependencies &&
+      packageJsonFile.dependencies.paintablejs
+    ) {
+      packageJsonFile.dependencies.paintablejs = mainPackageJson.version;
+      fs.writeFileSync(path, JSON.stringify(packageJsonFile, null, 2));
+    }
+  }
+});
 
 fs.writeFileSync(
   './packages/dist/package.json',
